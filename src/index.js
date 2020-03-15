12dc34a814c11b77e0944a560d293949213ee10a
Brian Hsu <brian@brianhsu.me>
2020-03-14 17:19:24 -0700

    Added sketchpad

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-var app = require('express')();
+const express = require('express')
+var app = express();
 var http = require('http').createServer(app);
 
+var NODE_MODULES = __dirname + '/../node_modules/';
+
 app.get('/', function(req, res) {
-    res.send('<h1> Hello world </h1>')
+    res.sendFile(__dirname + '/html/index.html')
 });
+
+// TODO: This is not best practice, but I don't know better.
+app.use('/sketchpad', express.static(__dirname + "/static/sketchpad/scripts/"));
 
 http.listen(3000, function() {
     console.log('listening on :3000')
